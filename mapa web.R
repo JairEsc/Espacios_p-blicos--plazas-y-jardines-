91489e9fa83901b02441b9a70e0af7edbc9c2582
JairEsc <p.escobar816@gmail.com>
2025-06-02 13:12:01 -0600

    color más fuerte a poligonos

--- mapa web.R
+++ mapa web.R
@@ -14,7 +14,7 @@ greenLeafIcon <- makeIcon(
 
 mapa_web=leaflet() |> 
   addTiles(options = leaflet::tileOptions(opacity =0.6))|>
-  addPolygons(data=parques_polygon |> st_zm() |> st_transform(st_crs("EPSG:4326")),fillColor = "green",opacity = 0.4,color = "green",weight = 1,fillOpacity = 0.2) |> 
+  addPolygons(data=parques_polygon |> st_zm() |> st_transform(st_crs("EPSG:4326")),fillColor = "#297008",opacity = 0.1,color = "#297008",weight = 1,fillOpacity = 0.9) |> 
   addMarkers(data=parques_jardines |> st_transform(st_crs("EPSG:4326")),icon =greenLeafIcon,group = "puntos"  )
 
 mapa_web
